7ad863452df810186898bb8bf92614bc62af9f94
Dmitry Khotyanovich <dmitry@stridertech.co.za>
2018-05-30 18:36:07 +0300

    refactoring.

--- TEST/DataLayer/DataLayer.swift
+++ TEST/DataLayer/DataLayer.swift
@@ -15,33 +15,44 @@ class DataLayer {
     func getList(_ completion: (([UserModel]?, Error?) -> Swift.Void)? ) -> [UserModel]? {
 		var users: [UserModel]?
 
-		let realm = try! Realm()
-		let results = realm.objects(UserModel.self)
+        // request for update new user data
+        networkLayer.users { models, error in
+            guard let models = models else {
+                completion?(nil, error)
+                return
+            }
+            completion?([UserModel](models), error)
+            
+            //try to save last updated data
+            self.saveData(data: models)
+        }
+
+        //get cached data
+        guard let realm = try? Realm() else {
+            log.warning("The Realm DB was not initialized")
+            return nil
+        }
+        let results = realm.objects(UserModel.self)
 		if !results.isEmpty {
 			users = Array(results)
 		}
-
-        if let completion = completion {
-            networkLayer.users { models, error in
-                guard let models = models else {
-                    completion(nil, error)
-                    return
-                }
-				let realm = try! Realm()
-				for model in models {
-					try! realm.write {
-						realm.add(model, update: true)
-					}
-				}
-                completion([UserModel](models), error)
-            }
-        }
         return users
     }
     
 	func geoposition(of userID: Int?, completion: (([GeoParams]?, Error?) -> Swift.Void)?) -> [GeoParams]? {
 		var geoposition: [GeoParams]?
 
+        //request for updated geo location of vehicles
+        networkLayer.vehicleLocations(userid: userID) { newGeoParams, error in
+            guard let newGeoParams = newGeoParams else {
+                completion?(nil, error)
+                return
+            }
+            completion?(newGeoParams, error)
+            //try to save last updated data
+            self.saveData(data: newGeoParams)
+        }
+        
 		let realm = try! Realm()
 		if let user = realm.object(ofType: UserModel.self, forPrimaryKey: userID), !user.vehicles.isEmpty {
 			let predicates: [NSPredicate] = user.vehicles.compactMap {
@@ -53,22 +64,23 @@ class DataLayer {
 				geoposition = Array(results)
 			}
 		}
-
-        if let completion = completion {
-            networkLayer.vehicleLocations(userid: userID) { newGeoParams, error in
-				guard let newGeoParams = newGeoParams else {
-					return
-				}
-				let realm = try! Realm()
-				for model in newGeoParams {
-					try! realm.write {
-						realm.add(model, update: true)
-					}
-				}
-                completion(newGeoParams, error)
+        return geoposition
+    }
+    
+    private func saveData<T>(data: [T]) where T: Object {
+        guard let realm = try? Realm() else  {
+            log.warning("DB was not initialized")
+            return
+        }
+        data.forEach{ model in
+            do {
+                try realm.write {
+                    realm.add(model, update: true)
+                }
+            } catch( let error) {
+                log.error("Realm throws error at writing user model. Error: \(error)")
             }
         }
-        return geoposition
     }
 }
 
